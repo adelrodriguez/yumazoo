664554c55b9cd88cf6d2901d89c907599ae51112
Adel Rodríguez <hello@adelrodriguez.com>
2024-01-15 12:21:25 -0400

    build: initial setup

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <main className="h-screen">
+        <h1 className="text-3xl text-white font-styled">Yumazoo</h1>
+      </main>
+    </>
+  );
+}
